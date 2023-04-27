9fe62d2543e9909eb5e95432e3455068a965b4a0
im222b <rudgnsdleh@naver.com>
2023-04-27 11:35:51 +0900

    게시판

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,11 +2,13 @@ package main;
 
 import java.util.Scanner;
 
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int num;
-
+		
+	
 		while (true) {
 			System.out.println("1. 로그인 기능");
 			System.out.println("2. 회원 기능");
